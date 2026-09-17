5b2e4d6cd3ed8cd65e806bad3b4564178b7c4075
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-17 19:26:50 +0700

    Pisahkan URI KRS operasional Dosen dari Master Akademik KRS

--- routes/dosen/dosen-aktif.php
+++ routes/dosen/dosen-aktif.php
@@ -11,14 +11,14 @@ Route::group(['prefix' => 'dosen', 'middleware' => ['checkUser:Dosen Aktif', 'do
     // Master Akademik Dosen: hak penuh dan controller yang sama dengan Admin.
     require __DIR__.'/master-akademik.php';
 
-    // Operasional akademik khusus Dosen.
+    // Operasional akademik khusus Dosen menggunakan URL terpisah agar tidak
+    // menimpa route Master Akademik dengan URI yang sama.
     Route::get('/akademik/jadwal',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'jadwal'])->name('akademik.jadwal');
     Route::post('/akademik/jadwal',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'simpanJadwal'])->name('akademik.jadwal.store');
     Route::patch('/akademik/jadwal/{code}',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'updateJadwal'])->name('akademik.jadwal.update');
     Route::get('/akademik/nilai',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'nilai'])->name('akademik.nilai');
     Route::patch('/akademik/nilai/{code}',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'updateNilai'])->name('akademik.nilai.update');
-    // Halaman KRS operasional Dosen diberi nama khusus agar tidak bentrok dengan Master Akademik KRS.
-    Route::get('/akademik/krs',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'krs'])->name('akademik.krs-operasional');
-    Route::post('/akademik/krs/{code}/approve',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'approveKrs'])->name('akademik.krs.approve');
-    Route::post('/akademik/krs/{code}/reject',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'rejectKrs'])->name('akademik.krs.reject');
+    Route::get('/akademik/krs-operasional',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'krs'])->name('akademik.krs-operasional');
+    Route::post('/akademik/krs-operasional/{code}/approve',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'approveKrs'])->name('akademik.krs-operasional.approve');
+    Route::post('/akademik/krs-operasional/{code}/reject',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'rejectKrs'])->name('akademik.krs-operasional.reject');
 });
