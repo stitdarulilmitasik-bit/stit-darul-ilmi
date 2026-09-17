3b22e28f77f060ba94d551305c0234c5a24f74b7
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-17 21:47:54 +0700

    Tambah route Surat Keterangan Aktif Kuliah mahasiswa

--- routes/mahasiswa/mahasiswa-aktif.php
+++ routes/mahasiswa/mahasiswa-aktif.php
@@ -8,7 +8,6 @@ Route::group(['prefix' => 'mahasiswa', 'middleware' => ['checkUser:Mahasiswa Akt
     Route::get('/home', [App\Http\Controllers\Private\Mahasiswa\DashboardController::class, 'index'])->name('dashboard-render');
     Route::get('/profile', [App\Http\Controllers\Private\Mahasiswa\RootController::class, 'renderProfile'])->name('profile-render');
     Route::patch('/profile', [App\Http\Controllers\Private\Mahasiswa\RootController::class, 'handleProfile'])->name('profile-handle');
-    
     Route::prefix('akademik')->name('akademik.')->group(function () {
         Route::get('/krs', [App\Http\Controllers\Private\Mahasiswa\AkademikController::class, 'krsRender'])->name('krs-render');
         Route::get('/krs/cetak', [App\Http\Controllers\Private\Mahasiswa\AkademikController::class, 'cetakKrs'])->name('krs-cetak');
@@ -26,7 +25,6 @@ Route::group(['prefix' => 'mahasiswa', 'middleware' => ['checkUser:Mahasiswa Akt
         Route::get('/nilai', [App\Http\Controllers\Private\Mahasiswa\AkademikController::class, 'nilai'])->name('nilai');
         Route::get('/nilai/{semester}', [App\Http\Controllers\Private\Mahasiswa\AkademikController::class, 'nilaiBySemester'])->name('nilai.semester');
     });
-    
     Route::prefix('keuangan')->name('keuangan.')->group(function () {
         Route::get('/tagihan', [App\Http\Controllers\Private\Mahasiswa\KeuanganController::class, 'tagihan'])->name('tagihan');
         Route::get('/tagihan/{id}/detail', [App\Http\Controllers\Private\Mahasiswa\KeuanganController::class, 'detailTagihan'])->name('tagihan.detail');
@@ -35,18 +33,18 @@ Route::group(['prefix' => 'mahasiswa', 'middleware' => ['checkUser:Mahasiswa Akt
         Route::get('/bukti-pembayaran', [App\Http\Controllers\Private\Mahasiswa\KeuanganController::class, 'buktiPembayaran'])->name('bukti-pembayaran');
         Route::post('/upload-bukti', [App\Http\Controllers\Private\Mahasiswa\KeuanganController::class, 'uploadBuktiPembayaran'])->name('upload-bukti');
     });
-    
     Route::prefix('layanan')->name('layanan.')->group(function () {
         Route::get('/transkrip', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'transkripNilai'])->name('transkrip');
         Route::get('/transkrip/cetak', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'cetakTranskrip'])->name('transkrip.cetak');
         Route::get('/surat-keterangan', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'suratKeterangan'])->name('surat-keterangan');
         Route::post('/ajukan-surat', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'ajukanSuratKeterangan'])->name('ajukan-surat');
+        Route::get('/surat-aktif-kuliah', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'suratAktifKuliah'])->name('surat-aktif-kuliah');
+        Route::post('/surat-aktif-kuliah', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'ajukanSuratAktifKuliah'])->name('ajukan-surat-aktif');
         Route::get('/legalisir', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'legalisirDokumen'])->name('legalisir');
         Route::post('/ajukan-legalisir', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'ajukanLegalisir'])->name('ajukan-legalisir');
         Route::get('/cuti', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'cutiAkademik'])->name('cuti');
         Route::post('/ajukan-cuti', [App\Http\Controllers\Private\Mahasiswa\LayananController::class, 'ajukanCuti'])->name('ajukan-cuti');
     });
-    
     Route::prefix('informasi')->name('informasi.')->group(function () {
         Route::get('/pengumuman', [App\Http\Controllers\Private\Mahasiswa\InformasiController::class, 'pengumuman'])->name('pengumuman');
         Route::get('/pengumuman/{id}', [App\Http\Controllers\Private\Mahasiswa\InformasiController::class, 'detailPengumuman'])->name('pengumuman.detail');
