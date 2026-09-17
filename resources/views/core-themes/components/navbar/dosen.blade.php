bb64dc6dc3e0c878b9adf487282a4f154926e126
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-17 18:08:43 +0700

    feat(dosen): expand academic navigation

--- resources/views/core-themes/components/navbar/dosen.blade.php
+++ resources/views/core-themes/components/navbar/dosen.blade.php
@@ -1,19 +1,14 @@
-<!-- Master Akademik untuk Dosen -->
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#navbar-dosen-akademik" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
         <span class="nav-link-icon d-md-none d-lg-inline-block">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M22 9l-10 -4l-10 4l10 4l10 -4"/><path d="M6 10.6v5.4a6 6 0 0 0 12 0v-5.4"/>
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M22 9l-10 -4l-10 4l10 4l10 -4"/><path d="M6 10.6v5.4a6 6 0 0 0 12 0v-5.4"/></svg>
         </span>
         <span class="nav-link-title">Akademik</span>
     </a>
     <div class="dropdown-menu">
-        <a class="dropdown-item" href="{{ route('dosen.akademik.master') }}">
-            <span class="nav-link-icon d-md-none d-lg-inline-block">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5a2.5 2.5 0 0 1 2.5 -2.5h13.5"/><path d="M6.5 2h13.5v20h-13.5a2.5 2.5 0 0 1 0 -5h13.5"/><path d="M9 7h6"/><path d="M9 11h6"/></svg>
-            </span>
-            Master Akademik
-        </a>
+        <a class="dropdown-item" href="{{ route('dosen.akademik.jadwal') }}">Jadwal Kuliah</a>
+        <a class="dropdown-item" href="{{ route('dosen.akademik.krs') }}">Persetujuan KRS</a>
+        <a class="dropdown-item" href="{{ route('dosen.akademik.nilai') }}">Input / Update Nilai</a>
+        <a class="dropdown-item" href="{{ route('dosen.akademik.master') }}">Master Akademik</a>
     </div>
 </li>
