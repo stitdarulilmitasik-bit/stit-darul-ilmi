b5200ada03818791ff1325780a112d4fc55886f4
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-17 12:12:58 +0700

    feat: add homepage CMS section model

---
+++ app/Models/Pengaturan/HomepageSection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Pengaturan;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\HasLogAktivitas;
+use App\Models\User;
+
+class HomepageSection extends Model
+{
+    use SoftDeletes, HasLogAktivitas;
+
+    protected $table = 'homepage_sections';
+    protected $guarded = [];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}
