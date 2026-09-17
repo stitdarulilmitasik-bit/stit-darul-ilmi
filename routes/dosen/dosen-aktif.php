a261182e6fdf2ae03a22e879d5cece90b9cf753e
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-17 18:23:31 +0700

    Tambah hak kelola mata kuliah untuk dosen

--- routes/dosen/dosen-aktif.php
+++ routes/dosen/dosen-aktif.php
@@ -9,6 +9,10 @@ Route::group(['prefix' => 'dosen', 'middleware' => ['checkUser:Dosen Aktif'], 'a
     Route::patch('/profile',[App\Http\Controllers\Private\Dosen\RootController::class, 'handleProfile'])->name('profile-handle');
 
     Route::get('/akademik/master',[App\Http\Controllers\Private\Dosen\AkademikController::class, 'index'])->name('akademik.master');
+    Route::post('/akademik/master/mata-kuliah',[App\Http\Controllers\Private\Dosen\AkademikController::class, 'storeMataKuliah'])->name('akademik.mata-kuliah.store');
+    Route::patch('/akademik/master/mata-kuliah/{code}',[App\Http\Controllers\Private\Dosen\AkademikController::class, 'updateMataKuliah'])->name('akademik.mata-kuliah.update');
+    Route::delete('/akademik/master/mata-kuliah/{code}',[App\Http\Controllers\Private\Dosen\AkademikController::class, 'deleteMataKuliah'])->name('akademik.mata-kuliah.delete');
+
     Route::get('/akademik/jadwal',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'jadwal'])->name('akademik.jadwal');
     Route::post('/akademik/jadwal',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'simpanJadwal'])->name('akademik.jadwal.store');
     Route::patch('/akademik/jadwal/{code}',[App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'updateJadwal'])->name('akademik.jadwal.update');
