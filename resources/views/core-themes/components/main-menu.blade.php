06ce185e24fd5290916bfd18f0dd15dacf60a02d
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-18 12:18:55 +0700

    Perbarui ikon navigasi front page agar lebih profesional

--- resources/views/core-themes/components/main-menu.blade.php
+++ resources/views/core-themes/components/main-menu.blade.php
@@ -4,9 +4,9 @@
         <a class="nav-link" href="{{ route('root.home-index') }}">
             <span class="nav-link-icon d-md-none d-lg-inline-block">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                    <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                    <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                    <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+                    <path d="M3 10.5 12 3l9 7.5" />
+                    <path d="M5 9.5V21h14V9.5" />
+                    <path d="M9 21v-6h6v6" />
                 </svg>
             </span>
             <span class="nav-link-title">Beranda</span>
@@ -38,10 +38,10 @@
         <a class="nav-link dropdown-toggle" href="#navbar-kemahasiswaan" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false">
             <span class="nav-link-icon d-md-none d-lg-inline-block">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                    <path d="M5 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+                    <path d="M16 3H5a2 2 0 0 0 -2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2 -2V8z" />
+                    <path d="M16 3v5h5" />
+                    <path d="M8 13h8" />
+                    <path d="M8 17h6" />
                 </svg>
             </span>
             <span class="nav-link-title">Kemahasiswaan</span>
@@ -77,12 +77,13 @@
             <span class="nav-link-icon d-md-none d-lg-inline-block">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
                     <path d="M3 21h18" />
-                    <path d="M19 21v-4" />
-                    <path d="M19 17a2 2 0 0 0 2 -2v-2a2 2 0 1 0 -4 0v2a2 2 0 0 0 2 2z" />
-                    <path d="M14 21v-14a3 3 0 0 0 -3 -3h-4a3 3 0 0 0 -3 3v14" />
-                    <path d="M9 17v4" />
-                    <path d="M8 13h2" />
-                    <path d="M8 9h2" />
+                    <path d="M5 21V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16" />
+                    <path d="M9 7h2" />
+                    <path d="M9 11h2" />
+                    <path d="M9 15h2" />
+                    <path d="M13 7h2" />
+                    <path d="M13 11h2" />
+                    <path d="M13 15h2" />
                 </svg>
             </span>
             <span class="nav-link-title">Institusi</span>
@@ -99,8 +100,8 @@
         <a class="nav-link" href="{{ route('root.kontak') }}">
             <span class="nav-link-icon d-md-none d-lg-inline-block">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                    <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
-                    <path d="M3 7l9 6l9 -6" />
+                    <rect x="3" y="5" width="18" height="14" rx="2" />
+                    <path d="m3 7 9 6 9-6" />
                 </svg>
             </span>
             <span class="nav-link-title">Kontak</span>
